272c579d07bce9c45ebbf065bb33206eeebfdd52
AlirezaPlus <99487660+AlirezaPlusOfficial@users.noreply.github.com>
2024-05-11 11:41:38 +0300

    Update quote.js

--- scripts/quote.js
+++ scripts/quote.js
@@ -1,1 +1,11 @@
+var quotes = [
+  "Hello World!",
+  "This is a sentence.",
+  "Programming is very interesting.",
+  "JavaScript is powerful.",
+  "I am running the desired code."
+];
 
+
+var randomIndex = Math.floor(Math.random() * sentences.length);
+document.getElementById("text").innerHTML = quotes[randomIndex];
